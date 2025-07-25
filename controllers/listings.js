7c5c8f18014cbb3e069974b922ca524a89784d7f
mrvaibhav06 <kushwahavaibhav150@gmail.com>
2025-07-25 10:00:53 +0530

    Initial commit

--- controllers/listings.js
+++ controllers/listings.js
@@ -27,15 +27,23 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
-  let url = req.file.path;
-  let filename = req.file.filename;
-  const newListing = new Listing(req.body.listing);
-  newListing.owner = req.user._id;
-  newListing.image = { url, filename };
-  await newListing.save();
-  req.flash("success", "New Listing Created!");
-  res.redirect("/listings");
+  try {
+    if (!req.file) {
+      req.flash("error", "Image upload failed or was not provided.");
+      return res.redirect("/listings/new");
+    }
+    const { path: url, filename } = req.file;
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    newListing.image = { url, filename };
+    await newListing.save();
+    req.flash("success", "New Listing Created!");
+    res.redirect("/listings");
+  } catch (err) {
+    next(err); // Pass to error handler middleware
+  }
 };
+
 module.exports.editListing = async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
